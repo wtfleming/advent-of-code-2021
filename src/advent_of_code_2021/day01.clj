20ba0ce039f5801d3750cdcea247625f67786c9d
Will Fleming <wfleming77@gmail.com>
2022-01-02 14:51:01 -0500

    add day 1 part 2 solution

--- src/advent_of_code_2021/day01.clj
+++ src/advent_of_code_2021/day01.clj
@@ -1,7 +1,7 @@
 (ns advent-of-code-2021.day01
   (:require [advent-of-code-2021.core :as core]))
 
-(defn- do-day1-part-1 [input previous num-increased]
+(defn- num-increasing [input previous num-increased]
   (if (seq input)
     (let [increased? (> (first input) previous)]
       (if increased?
@@ -12,10 +12,20 @@
 
 (defn day1-part-1 [file-name]
   (let [input (core/input->int-seq file-name)]
-    (do-day1-part-1 (rest input) (first input) 0)))
+    (num-increasing (rest input) (first input) 0)))
+
+
+(defn day1-part-2 [file-name]
+  (let [input (->> (core/input->int-seq file-name)
+                   (partition 3 1) ; create a sliding window
+                   (map #(apply + %)))] ; sum it
+    (num-increasing (rest input) (first input) 0)))
 
 
 (comment
   (day1-part-1 "day1-example.txt") ;; => 7
   (day1-part-1 "day1.txt")
+
+  (day1-part-2 "day1-example.txt")
+  (day1-part-2 "day1.txt")
 )
