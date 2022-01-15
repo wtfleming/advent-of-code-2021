2bd105cc64fa88da10a862fbb0f3afe733e958d5
Will Fleming <wfleming77@gmail.com>
2022-01-15 15:06:59 -0500

    implement day 2 part 2

--- src/advent_of_code_2021/day02.clj
+++ src/advent_of_code_2021/day02.clj
@@ -1,7 +1,7 @@
 (ns advent-of-code-2021.day02
   (:require [advent-of-code-2021.core :as core]))
 
-(defn update-position [line current-pos]
+(defn update-position-part-1 [line current-pos]
   (let [[direction amt-str] (clojure.string/split line #" ")
         amt (Integer/parseInt amt-str)]
     (condp = direction
@@ -10,14 +10,38 @@
       "down" (update current-pos :y + amt))))
 
 
-(defn calculate-position [lines acc]
+(defn calculate-position-part-1 [lines acc]
   (if (seq lines)
-    (recur (rest lines) (update-position (first lines) acc))
+    (recur (rest lines) (update-position-part-1 (first lines) acc))
     acc))
 
 (defn day2-part-1 [file-name]
   (let [file (core/read-input file-name)
-        final-pos (calculate-position (clojure.string/split-lines file) {:x 0 :y 0})]
+        final-pos (calculate-position-part-1 (clojure.string/split-lines file) {:x 0 :y 0})]
+    (* (:x final-pos) (:y final-pos))))
+
+
+(defn update-position-part-2 [line current-pos]
+  (let [[direction amt-str] (clojure.string/split line #" ")
+        amt (Integer/parseInt amt-str)]
+    (condp = direction
+      "forward" (-> current-pos
+                    (update :x + amt)
+                    (update :y + (* amt (:aim current-pos))))
+      "up" (update current-pos :aim - amt)
+      "down" (update current-pos :aim + amt))))
+
+
+
+(defn calculate-position-part-2 [lines acc]
+  (if (seq lines)
+    (recur (rest lines) (update-position-part-2 (first lines) acc))
+    acc))
+
+
+(defn day2-part-2 [file-name]
+  (let [file (core/read-input file-name)
+        final-pos (calculate-position-part-2 (clojure.string/split-lines file) {:x 0 :y 0 :aim 0})]
     (* (:x final-pos) (:y final-pos))))
 
 
@@ -26,5 +50,7 @@
   (day2-part-1 "day2-example.txt")
   (day2-part-1 "day2.txt")
 
+  (day2-part-2 "day2-example.txt")
+  (day2-part-2 "day2.txt")
 )
 
